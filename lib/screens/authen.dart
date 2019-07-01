16e0abf214f7b3c57268516cc280007ed47d4efe
masterUNG <phrombutr@gmail.com>
2019-07-01 15:27:56 +0700

    Image OK

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/material.dart' as prefix0;
 
 class Authen extends StatefulWidget {
   @override
@@ -7,8 +7,30 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  Widget showLogo() {
+    return Image.asset(
+      'images/logo.png',
+      fit: BoxFit.contain,
+    );
+  }
+
+  Widget showText() {
+    return Text('Authen');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Authen');
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(top: 60.0),
+        alignment: Alignment.topCenter,
+        child: Column(
+          children: <Widget>[
+            showLogo(),
+            showText(),
+          ],
+        ),
+      ),
+    );
   }
 }
