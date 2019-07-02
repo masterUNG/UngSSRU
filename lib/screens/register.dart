f5b9c45890748a5b712ba7d192795b5ddea915de
masterUNG <phrombutr@gmail.com>
2019-07-02 16:41:57 +0700

    Finish 2July

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -8,6 +8,7 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   // Explicit
   final formKey = GlobalKey<FormState>();
+  String nameString, emailString, passwordString;
 
   // Method
   Widget nameText() {
@@ -22,12 +23,19 @@ class _RegisterState extends State<Register> {
           size: 36.0,
           color: Colors.pink[400],
         ),
-      ),
+      ),validator: (String value){
+        if (value.isEmpty) {
+          return 'Please Fill Name in Blank';
+        }
+      },onSaved: (String value){
+        nameString = value;
+      },
     );
   }
 
   Widget emailText() {
-    return TextFormField(keyboardType: TextInputType.emailAddress,
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
       decoration: InputDecoration(
         labelText: 'Email :',
         labelStyle: TextStyle(color: Colors.blue),
@@ -38,7 +46,13 @@ class _RegisterState extends State<Register> {
           size: 36.0,
           color: Colors.blue,
         ),
-      ),
+      ),validator: (String value){
+        if (!((value.contains('@')) && (value.contains('.')))) {
+          return 'Type Email Format';
+        }
+      },onSaved: (String value){
+        emailString = value;
+      },
     );
   }
 
@@ -54,7 +68,13 @@ class _RegisterState extends State<Register> {
           size: 36.0,
           color: Colors.green,
         ),
-      ),
+      ),validator: (String value){
+        if (value.length <= 5) {
+          return 'Pass Much More 6 Charactor';
+        }
+      },onSaved: (String value){
+        passwordString = value;
+      },
     );
   }
 
@@ -63,38 +83,46 @@ class _RegisterState extends State<Register> {
       icon: Icon(Icons.cloud_upload),
       onPressed: () {
         print('Upload');
+        if (formKey.currentState.validate()) {
+          formKey.currentState.save();
+          print('Name = $nameString, Email = $emailString, Pass = $passwordString');
+        }
       },
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(resizeToAvoidBottomPadding: false,
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
       appBar: AppBar(
         backgroundColor: Colors.green[800],
         title: Text('Register'),
         actions: <Widget>[uploadButton()],
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('images/bg.png'), fit: BoxFit.cover),
-        ),
-        alignment: Alignment.topCenter,
-        padding: EdgeInsets.only(top: 60.0),
+      body: Form(
+        key: formKey,
         child: Container(
-          padding: EdgeInsets.all(10.0),
           decoration: BoxDecoration(
-            color: Color.fromRGBO(255, 255, 255, 0.8),
+            image: DecorationImage(
+                image: AssetImage('images/bg.png'), fit: BoxFit.cover),
           ),
-          width: 300.0,
-          height: 300.0,
-          child: Column(
-            children: <Widget>[
-              nameText(),
-              emailText(),
-              passwordText(),
-            ],
+          alignment: Alignment.topCenter,
+          padding: EdgeInsets.only(top: 60.0),
+          child: Container(
+            padding: EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(255, 255, 255, 0.8),
+            ),
+            width: 300.0,
+            height: 300.0,
+            child: Column(
+              children: <Widget>[
+                nameText(),
+                emailText(),
+                passwordText(),
+              ],
+            ),
           ),
         ),
       ),
