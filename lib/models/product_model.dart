6deae6aaadf163b9fd0baae6d45ad855b3686f31
masterUNG <phrombutr@gmail.com>
2019-08-05 15:52:45 +0700

    finish 5Aug

---
+++ lib/models/product_model.dart
@@ -0,0 +1,10 @@
+class ProductModel {
+  
+  // Field
+  String name, detail, url;
+
+  // Constructor
+  ProductModel(this.name, this.detail, this.url);
+
+
+}
--- lib/screens/show_product.dart
+++ lib/screens/show_product.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ung_ssru/models/product_model.dart';
 
 class ShowProduct extends StatefulWidget {
   @override
@@ -9,42 +10,54 @@ class ShowProduct extends StatefulWidget {
 }
 
 class _ShowProductState extends State<ShowProduct> {
-
   // Explicit
   Firestore fireStore = Firestore.instance;
   StreamSubscription<QuerySnapshot> subscription;
   List<DocumentSnapshot> snapshots;
-
+  List<ProductModel> productModels = [];
 
   // Method
   @override
-  void initState() { 
+  void initState() {
     super.initState();
     readFireStore();
   }
 
-  Future<void> readFireStore()async{
-
+  Future<void> readFireStore() async {
     CollectionReference collectionReference = fireStore.collection('Product');
-    subscription = await collectionReference.snapshots().listen((dataSnapshop){
+    subscription = await collectionReference.snapshots().listen((dataSnapshop) {
       snapshots = dataSnapshop.documents;
 
       for (var snapshot in snapshots) {
-        
         String nameProduct = snapshot.data['Name'];
         print('nameProduct = $nameProduct');
 
+        String detailProduct = snapshot.data['Detial'];
+        String urlProduct = snapshot.data['Url'];
+
+        ProductModel productModel =
+            ProductModel(nameProduct, detailProduct, urlProduct);
+
+        setState(() {
+          productModels.add(productModel);
+        });
       }
-
     });
-
   }
 
+  Widget showListProduct() {
+    return ListView.builder(
+      itemCount: productModels.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Text(productModels[index].name);
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('This is ShowProduce'),
+      child: showListProduct(),
     );
   }
 }
